8f46b23c6c1cd0cd6a5114ada49f1ad68735dd7b
Michael Truog <mjtruog@gmail.com>
2016-09-14 21:25:33 -0700

    Version 1.5.3.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceHttpClient do
 
   def project do
     [app: :cloudi_service_http_client,
-     version: "1.5.1",
+     version: "1.5.3",
      language: :erlang,
      description: description,
      package: package,
@@ -11,9 +11,9 @@ defmodule CloudIServiceHttpClient do
   end
 
   defp deps do
-    [{:trie, "~> 1.5.1"},
-     {:uuid, "~> 1.5.1", hex: :uuid_erl},
-     {:cloudi_core, "~> 1.5.1"}]
+    [{:trie, "~> 1.5.3"},
+     {:uuid, "~> 1.5.3", hex: :uuid_erl},
+     {:cloudi_core, "~> 1.5.3"}]
   end
 
   defp description do
