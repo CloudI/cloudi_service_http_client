3a9ff0787b3d1062b5be6dea93e0968860fccad1
Michael Truog <mjtruog@gmail.com>
2017-03-28 20:53:44 -0700

    Version 1.7.0.

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
+#-*-Mode:elixir;coding:utf-8;tab-width:2;c-basic-offset:2;indent-tabs-mode:()-*-
+# ex: set ft=elixir fenc=utf-8 sts=2 ts=2 sw=2 et nomod:
+
 defmodule CloudIServiceHttpClient do
   use Mix.Project
 
   def project do
     [app: :cloudi_service_http_client,
-     version: "1.6.0",
+     version: "1.7.0",
      language: :erlang,
      description: description(),
      package: package(),
@@ -11,9 +14,9 @@ defmodule CloudIServiceHttpClient do
   end
 
   defp deps do
-    [{:trie, "~> 1.6.0"},
-     {:uuid, "~> 1.6.0", hex: :uuid_erl},
-     {:cloudi_core, "~> 1.6.0"}]
+    [{:trie, "~> 1.7.0"},
+     {:uuid, "~> 1.7.0", hex: :uuid_erl},
+     {:cloudi_core, "~> 1.7.0"}]
   end
 
   defp description do
