97cd1f3ad80f5152b9a577420abb7ca2161f9117
Michael Truog <mjtruog@protonmail.com>
2018-09-15 21:08:57 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,17 +6,32 @@ defmodule CloudIServiceHttpClient do
 
   def project do
     [app: :cloudi_service_http_client,
-     version: "1.7.3",
+     version: "1.7.4",
      language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
   end
 
   defp deps do
-    [{:trie, "~> 1.7.3"},
-     {:uuid, "~> 1.7.3", hex: :uuid_erl},
-     {:cloudi_core, "~> 1.7.3"}]
+    [{:trie, "~> 1.7.4"},
+     {:uuid, "~> 1.7.4", hex: :uuid_erl},
+     {:cloudi_core, "~> 1.7.4"}]
   end
 
   defp description do
